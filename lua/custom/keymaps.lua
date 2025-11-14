e1cc39f247827ae164a9e70b4878d866d7f55a15
Alejandro Velasco <alejandrovelasco94@gmail.com>
2025-11-13 18:41:58 -0600

    updates to config and adds lazygit

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -3,3 +3,6 @@ vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChatToggle<CR>', { desc = 'Toggle
 
 -- Open Copilot Chat in a floating window with a prompt
 vim.keymap.set('v', '<leader>cce', '<cmd>CopilotChatExplain<CR>', { desc = 'Explain CopilotChat (visual)' })
+
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("Shift-y>")', { expr = true, replace_keycodes = false })
